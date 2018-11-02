54980ec1f462070edc6e0f673303f02c7de99712
wangpeng <wangpeng@yestae.com>
2018-11-02 14:31:43 +0800

    完善ribbon方式调用

--- spring-could-test/service-ribbon/src/main/java/com/yestae/ribbon/service/RemoteRibbonService.java
+++ spring-could-test/service-ribbon/src/main/java/com/yestae/ribbon/service/RemoteRibbonService.java
@@ -1,8 +1,10 @@
 package com.yestae.ribbon.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class RemoteRibbonService {
 	@Autowired
 	private RestTemplate restTemplate;
