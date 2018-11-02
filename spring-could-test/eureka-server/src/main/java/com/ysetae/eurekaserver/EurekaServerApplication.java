85b39df5e065112a9d32b02f66e1b1fd4bd7e541
wangpeng <wangpeng@yestae.com>
2018-11-02 11:05:32 +0800

    重命名包

--- spring-could-test/eureka-server/src/main/java/com/ysetae/eurekaserver/EurekaServerApplication.java
+++ spring-could-test/eureka-server/src/main/java/com/ysetae/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.eureka.server;
+package com.ysetae.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
